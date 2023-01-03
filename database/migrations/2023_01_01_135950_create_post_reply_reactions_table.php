4a2be65ac1ce70e734229a77a4e8c81650ac04bf
unknown <suwaiwaihnin20@gmail.com>
2023-01-03 12:37:48 +0630

    add template and database setup

---
+++ database/migrations/2023_01_01_135950_create_post_reply_reactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_reply_reactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete;
+            $table->foreignId('post_reaction_id')->constrained('post_reactions')->onDelete('cascade');
+            $table->integer('reply_like')->nulable();
+            $table->longText('reply_comment')->nullable();
+            $table->integer('reply_user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_reply_reactions');
+    }
+};
